83adebfa8d1197d35f1fecfbc6ba35ffb289fd8d
Alperen <138169703+NAlpN@users.noreply.github.com>
2024-07-15 09:38:00 +0300

    PyTorch ile Çiçek Sınıflandırma Projesi

---
+++ 8- PyTorch/CicekSiniflandirma.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+
+train_dir = 'dataset/train'
+test_dir = 'dataset/test'
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+train_dataset = ImageFolder(root=train_dir, transform=transform)
+test_dataset = ImageFolder(root=test_dir, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+class flowerCNN(nn.Module):
+    def __init__(self):
+        super(flowerCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, 1)
+        self.fc1 = nn.Linear(128*14*14, 512)
+        self.fc2 = nn.Linear(512, 5)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(2)
+        self.dropout = nn.Dropout(0.5)
+        
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.maxpool(x)
+        x = self.relu(self.conv2(x))
+        x = self.maxpool(x)
+        x = self.relu(self.conv3(x))
+        x = self.maxpool(x)
+        x = x.view(-1, 128*14*14)
+        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.fc2(x)
+        return x
+
+model = flowerCNN()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    
+    print(f'Epoch: [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}')
+
+
+model.eval()
+correct = 0
+total = 0
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Test görüntüleri üzerindeki modelin doğruluğu: {100 * correct / total:.2f}%')
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+dataiter = iter(test_loader)
+images, labels = next(dataiter)
+imshow(torchvision.utils.make_grid(images))
